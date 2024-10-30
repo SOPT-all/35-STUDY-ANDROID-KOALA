11b093a88bf962191bf1eae9342002101128d5ab
t1nm1ksun <minsuk07672@naver.com>
2024-10-30 20:22:06 +0900

    [2주차] 프로그래머스_Lv1_실패율_신민석

--- minseok-shin/1주차/프로그래머스_Lv1_숫자문자열과영단어.kt
+++ minseok-shin/1주차/프로그래머스_Lv1_숫자문자열과영단어.kt
@@ -1,6 +1,6 @@
 package com.example.andsoptkoala
 
-class Solution1 {
+class Solution1{
     fun solution(s: String): Int {
         var answer: Int = 0
         val answerString = s.replace(oldValue = "zero", newValue = "0")
@@ -14,30 +14,6 @@ class Solution1 {
             .replace(oldValue = "eight", newValue = "8")
             .replace(oldValue = "nine", newValue = "9")
         answer = answerString.toInt()
-        return answer
-    }
-}
-
-
-class Solution2 {
-    fun solution(s: String): IntArray {
-        var answer = intArrayOf()
-        val numbers = s.replace("{", "").replace("}", "")
-            .split(",")
-            .filter { it.isNotEmpty() }
-            .map { it.toInt() }
-
-        val map = mutableMapOf<Int, Int>()
-        for (num in numbers) {
-            map[num] = map.getOrDefault(num, 0) + 1
-        }
-
-        val sortedTuple = map.toList()
-            .sortedByDescending { it.second }
-            .map { it.first }
-
-        answer = sortedTuple.toIntArray()
-
         return answer
     }
 }
---
+++ minseok-shin/2주차/프로그래머스_Lv1_실패율.kt
@@ -0,0 +1,21 @@
+package com.example.andsoptkoala
+
+class Solution {
+    fun solution(N: Int, stages: IntArray): IntArray {
+        val ratio = mutableListOf<Pair<Int, Double>>()
+
+        for (cnt in 1..N) {
+            val all = stages.count { it >= cnt }
+            val miss = stages.count { it == cnt }
+            val failRate = if(all!=0) miss.toDouble() / all.toDouble() else 0.0
+            ratio.add(Pair(cnt, failRate))
+
+        }
+
+        val answer = ratio.sortedWith(compareByDescending<Pair<Int, Double>> { it.second }.thenBy { it.first })
+            .map { it.first }
+            .toIntArray()
+
+        return answer
+    }
+}
