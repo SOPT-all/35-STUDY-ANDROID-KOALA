9910ff09b22d92bc64fa0669f8c11035d972bb26
MinJae Han <hanminjae131@gmail.com>
2024-11-05 11:33:43 +0900

    [3주차] 백준_실버_안전영역_한민재

--- minjae-han/week3/백준_실버_안전영역_한민재.kt
+++ minjae-han/week3/백준_실버_안전영역_한민재.kt
@@ -1,56 +1,22 @@
-private val dx = intArrayOf(-1, 1, 0, 0)
-private val dy = intArrayOf(0, 0, -1, 1)
-
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
+    val map = Array(n) { readLine().split(" ").map { it.toInt() }.toIntArray() }
+    val dx = arrayOf(-1, 1, 0, 0)
+    val dy = arrayOf(0, 0, -1, 1)
     
-    var minHeight = 100
-    var maxHeight = 1
-    val area = Array(n) { 
-        readLine().split(" ").map { 
-            it.toInt().also { h ->
-                minHeight = minOf(minHeight, h)
-                maxHeight = maxOf(maxHeight, h)
-            }
-        }.toIntArray()
+    fun dfs(h: Int, x: Int, y: Int, visited: Array<BooleanArray>): Boolean {
+        if (x !in 0 until n || y !in 0 until n || visited[x][y] || map[x][y] <= h) return false
+        visited[x][y] = true
+        repeat(4) { dfs(h, x + dx[it], y + dy[it], visited) }
+        return true
     }
     
-
-    val visited = Array(n) { BooleanArray(n) }
-    var maxSafeAreas = 1 
-    
-    for (height in minHeight until maxHeight) {
-        var count = 0
-        
-        for (i in 0 until n) {
-            visited[i].fill(false)
+    println((0..100).maxOf { h ->
+        var cnt = 0
+        val visited = Array(n) { BooleanArray(n) }
+        for (i in 0 until n) for (j in 0 until n) {
+            if (dfs(h, i, j, visited)) cnt++
         }
-        
-        for (i in 0 until n) {
-            for (j in 0 until n) {
-                if (!visited[i][j] && area[i][j] > height) {
-                    dfs(i, j, height, area, visited, n)
-                    count++
-                }
-            }
-        }
-        
-        maxSafeAreas = maxOf(maxSafeAreas, count)
-    }
-    
-    print(maxSafeAreas)
-}
-
-private fun dfs(x: Int, y: Int, height: Int, area: Array<IntArray>, visited: Array<BooleanArray>, n: Int) {
-    visited[x][y] = true
-    
-    for (i in 0..3) {
-        val nx = x + dx[i]
-        val ny = y + dy[i]
-        
-        if (nx in 0 until n && ny in 0 until n && 
-            !visited[nx][ny] && area[nx][ny] > height) {
-            dfs(nx, ny, height, area, visited, n)
-        }
-    }
+        cnt
+    })
 }
