6a3f885834c7235b8bedb7caaaa1a7728d995d9a
MinJae Han <hanminjae131@gmail.com>
2024-12-13 22:09:48 +0900

    [6주차] 프로그래머스_LV3_파괴되지않은건물_한민재

--- minjae-han/week6/프로그래머스_LV3_파괴되지않은건물_한민재.kt
+++ minjae-han/week6/프로그래머스_LV3_파괴되지않은건물_한민재.kt
@@ -1,14 +1,36 @@
 class Solution {
-   fun solution(board: Array<IntArray>, skill: Array<IntArray>) = with(Array(board.size + 1) { IntArray(board[0].size + 1) }) {
-       skill.forEach { (t, r1, c1, r2, c2, d) ->
-           val damage = if (t == 1) -d else d
-           this[r1][c1] += damage
-           this[r1][c2 + 1] -= damage
-           this[r2 + 1][c1] -= damage
-           this[r2 + 1][c2 + 1] += damage
-       }
-       (0..board.size).forEach { i -> (1..board[0].size).forEach { j -> this[i][j] += this[i][j - 1] } }
-       (1..board.size).forEach { i -> (0..board[0].size).forEach { j -> this[i][j] += this[i - 1][j] } }
-       board.indices.sumOf { i -> board[0].indices.count { j -> board[i][j] + this[i][j] > 0 } }
-   }
+    fun solution(board: Array<IntArray>, skill: Array<IntArray>): Int {
+        val sum = Array(board.size + 1) { IntArray(board[0].size + 1) }
+        
+        for (s in skill) {
+            val degree = if (s[0] == 1) -s[5] else s[5]
+            val (r1, c1, r2, c2) = s.slice(1..4)
+            
+            sum[r1][c1] += degree
+            sum[r1][c2 + 1] -= degree
+            sum[r2 + 1][c1] -= degree
+            sum[r2 + 1][c2 + 1] += degree
+        }
+        
+        for (r in sum.indices) {
+            for (c in 1 until sum[0].size) {
+                sum[r][c] += sum[r][c-1]
+            }
+        }
+        
+        for (c in sum[0].indices) {
+            for (r in 1 until sum.size) {
+                sum[r][c] += sum[r-1][c]
+            }
+        }
+        
+        var count = 0
+        for (i in board.indices) {
+            for (j in board[0].indices) {
+                if (board[i][j] + sum[i][j] > 0) count++
+            }
+        }
+        
+        return count
+    }
 }
