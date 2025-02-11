a2720d8c5de5ad7677edff4bf0ec4e6e528335ee
Jong <jm991014@gmail.com>
2025-02-12 05:28:47 +0900

    [9주차] 프로그래머스_Lv1_가장많이받은선물_김종명

---
+++ jongmyoung-kim/week9/프로그래머스_Lv1_가장많이받은선물.kt
@@ -0,0 +1,26 @@
+package week9
+
+fun solution(friends: Array<String>, gifts: Array<String>): Int {
+    val giveMap = HashMap<String, List<String>>()
+    val receiveMap = HashMap<String, List<String>>()
+    val nextMonth = HashMap<String, Int>()
+
+    gifts.forEach {
+        val (give, receive) = it.split(" ")
+        giveMap[give] = giveMap.getOrDefault(give, emptyList()) + receive
+        receiveMap[receive] = receiveMap.getOrDefault(receive, emptyList()) + give
+    }
+
+    friends.forEach { giver ->
+        val given = giveMap[giver].orEmpty()
+        val received = receiveMap[giver].orEmpty()
+
+        friends.filter { it != giver }.forEach { friend ->
+            if (given.count { it == friend } > received.count { it == friend } ||
+                given.count { it == friend } == received.count { it == friend } &&
+                (given.size - received.size) > (giveMap[friend].orEmpty().size - receiveMap[friend].orEmpty().size))
+                nextMonth[giver] = nextMonth.getOrDefault(giver, 0) + 1
+        }
+    }
+    return nextMonth.values.maxByOrNull { it } ?: 0
+}
