5c51202da98d185e69c8e058a1592abe51ae6681
Jong <jm991014@gmail.com>
2025-03-19 16:06:23 +0900

    [13주차] 프로그래머스_Lv2_가장큰수_김종명

---
+++ jongmyoung-kim/week13/프로그래머스_Lv2_가장큰수.kt
@@ -0,0 +1,9 @@
+package week13
+
+fun solution(numbers: IntArray): String {
+    val result = numbers
+        .map { it.toString() }
+        .sortedWith { num1, num2 -> (num2 + num1).compareTo(num1 + num2) }
+        .joinToString("")
+    return if (result[0] == '0') "0" else result
+}
