cfc1c3e0fe80c826298a46aa8c40447d46052338
Jong <jm991014@gmail.com>
2025-03-19 16:06:00 +0900

    [13주차] 프로그래머스_Lv1_최소직사각형_김종명

---
+++ jongmyoung-kim/week13/프로그래머스_Lv1_최소직사각형.kt
@@ -0,0 +1,12 @@
+package week13
+
+fun solution(sizes: Array<IntArray>): Int {
+    var width = 0
+    var height = 0
+    sizes.forEach {
+        it.sort()
+        width = width.coerceAtLeast(it[0])
+        height = height.coerceAtLeast(it[1])
+    }
+    return (width * height)
+}
