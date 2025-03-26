6fe0765610dd45c64ff747f94d43d7948a13e7de
beom84 <cjja00@konkuk.ac.kr>
2025-03-26 20:47:56 +0900

    프로그래머스_Lv1_안전지대_이승범_수정!

--- seungbum/week14/프로그래머스_Lv1_안전지대_이승범.kt
+++ seungbum/week14/프로그래머스_Lv1_안전지대_이승범.kt
@@ -4,21 +4,19 @@ class Week14Second {
     fun solution(board: Array<IntArray>): Int {
         val n = board.size
         var count = 0
+        val dir = intArrayOf(1, 0, -1)
+
         val wideBoard = Array(n + 2) { IntArray(n + 2) { 0 } }
         for (i in board.indices) {
             for (j in board.indices) {
                 if (board[i][j] == 1) {
                     val x = i + 1
                     val y = j + 1
-                    wideBoard[x][y] = 1
-                    wideBoard[x - 1][y] = 1
-                    wideBoard[x + 1][y] = 1
-                    wideBoard[x][y - 1] = 1
-                    wideBoard[x][y + 1] = 1
-                    wideBoard[x - 1][y + 1] = 1
-                    wideBoard[x - 1][y - 1] = 1
-                    wideBoard[x + 1][y - 1] = 1
-                    wideBoard[x + 1][y + 1] = 1
+                    for (dx in dir) {
+                        for (dy in dir) {
+                            wideBoard[x + dx][y + dy] = 1
+                        }
+                    }
                 }
             }
         }
